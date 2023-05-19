3c12adb05dd16a8eabb3b7e0d5d4ea3902787fb5
misaelricardo <misael24ricardo@gmail.com>
2023-05-20 01:50:31 +0700

    sebagian dashboard + routing

--- resources/views/newOrder.blade.php
+++ resources/views/newOrder.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <form action="{{ url('dashboard/submitOrder') }}" method="POST">
+    <form action="/input" method="POST">
         <div class="mb-3">
             {{ csrf_field() }}
             <label for="customerName" class="form-label">Customer's Name</label>
