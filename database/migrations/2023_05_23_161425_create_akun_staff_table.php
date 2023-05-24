0b3e4cbd04d20011a66797e613b50976a6b232d7
Arisudana <made.arisudana@gmail.com>
2023-05-24 15:54:35 +0700

    Feat: Migration & Seeding initial admin & staff account

--- database/migrations/2023_05_23_161425_create_akun_staff_table.php
+++ database/migrations/2023_05_23_161425_create_akun_staff_table.php
@@ -9,19 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('akun_staff', function (Blueprint $table) {
             $table->id();
             $table->string('username', 55)->unique();
             $table->string('password', 255);
+            $table->string('firstName', 55);
+            $table->string('lastName', 55);
+            $table->string('email', 255);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('akun_staff');
     }
