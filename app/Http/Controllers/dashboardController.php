4c22c95ece4fe4fd362820269ba77f3a96328b3b
misaelricardo <misael24ricardo@gmail.com>
2023-05-25 14:06:55 +0700

    Orders Count di Dashboard

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -7,28 +7,30 @@ use Illuminate\Support\Facades\DB;
 
 class dashboardController extends Controller
 {
-    public function viewDashboard()
-    {
-        $totalOrders = DB::connection('mysql')->table('orders')->count();
+    public function viewDashboard(){
+        return view('dashboard');
+}
+    public function orderTotals(){
+    $totalOrders = DB::connection('mysql')->table('orders')->count();
 
-        $currentMonth = now()->format('Y-m');
-        $currentMonthOrders = DB::connection('mysql')
-            ->table('orders')
-            ->whereRaw('DATE_FORMAT(orderDate, "%Y-%m") = ?', [$currentMonth])
-            ->count();
+    $currentMonth = now()->format('Y-m');
+    $currentMonthOrders = DB::connection('mysql')
+        ->table('orders')
+        ->whereRaw('DATE_FORMAT(orderDate, "%Y-%m") = ?', [$currentMonth])
+        ->count();
 
-        $lastMonth = now()->subMonth()->format('Y-m');
-        $lastMonthOrders = DB::connection('mysql')
-            ->table('orders')
-            ->whereRaw('DATE_FORMAT(orderDate, "%Y-%m") = ?', [$lastMonth])
-            ->count();
+    $lastMonth = now()->subMonth()->format('Y-m');
+    $lastMonthOrders = DB::connection('mysql')
+        ->table('orders')
+        ->whereRaw('DATE_FORMAT(orderDate, "%Y-%m") = ?', [$lastMonth])
+        ->count();
 
-        $orderTotals = [
-            'totalOrders' => $totalOrders,
-            'currentMonthOrders' => $currentMonthOrders,
-            'lastMonthOrders' => $lastMonthOrders,
-        ];
+    $orderTotals = [
+        'totalOrders' => $totalOrders,
+        'currentMonthOrders' => $currentMonthOrders,
+        'lastMonthOrders' => $lastMonthOrders,
+    ];
 
-        return view('dashboard', compact('orderTotals'));
+    return view('dashboard', compact('orderTotals'));
     }
 }
