c7de8fca6343607fd224332a735fa4dfdad9109e
Alif Des <alifdestiano47@gmail.com>
2023-06-10 03:53:21 +0700

    made anjay

--- app/Http/Controllers/SettingsAdminController.php
+++ app/Http/Controllers/SettingsAdminController.php
@@ -25,13 +25,6 @@ class SettingsAdminController extends Controller
 
         return view('SettingsAdmin', compact('role'));
     }
-        return view('SettingsAdmin');
-    }
-
-    public function SettingsEditProfile()
-    {
-        return view('SettingsEditProfile');
-    }
 
     public function SettingsListStaff()
     {
@@ -47,4 +40,44 @@ class SettingsAdminController extends Controller
     {
         return view('SettingsOrder');
     }
+
+    public function SettingsEditProfile($id)
+    {
+        $user = Auth::user();
+        $isAdmin = AkunAdmin::where('username', $user->username)->exists();
+        $isStaff = AkunStaff::where('username', $user->username)->exists();
+
+        if ($isAdmin) {
+            $akun_admin = DB::table('akun_admin')->where('username',$id)->get();
+            return view('SettingsEditProfile',['akun_admin' => $akun_admin]);
+        } elseif ($isStaff) {
+            $akun_staff = DB::table('akun_staff')->where('username',$id)->get();
+        return view('SettingsEditProfile',['akun_staff' => $akun_staff]);
+    }
+    }
+
+    public function SubmitEdit(Request $request)
+    {
+        $user = Auth::user();
+        $isAdmin = AkunAdmin::where('username', $user->username)->exists();
+        $isStaff = AkunStaff::where('username', $user->username)->exists();
+
+        if ($isAdmin) {
+            DB::table('akun_admin')->where('username',$request->id)->update([
+                'lastName' => $request->lastName,
+                'firstName' => $request->firstName,
+                'email' => $request->email
+            ]);
+        } elseif ($isStaff) {
+            DB::table('akun_staff')->where('username',$request->id)->update([
+                'lastName' => $request->lastName,
+                'firstName' => $request->firstName,
+                'email' => $request->email
+            ]);
+        }
+
+
+        return redirect('/settings');
+    }
+
 }
